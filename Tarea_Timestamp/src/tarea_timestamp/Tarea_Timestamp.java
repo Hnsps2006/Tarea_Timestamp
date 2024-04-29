0cd551366329b78c6b90bbbd171e0f6d3facfb18
Hnsps2006 <elizacasco1@hotmail.com>
2024-04-28 20:26:33 -0600

    tare done

---
+++ Tarea_Timestamp/src/tarea_timestamp/Tarea_Timestamp.java
@@ -0,0 +1,161 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea_timestamp;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author eliza
+ */
+public class Tarea_Timestamp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        Timestamp fecha1 = new Timestamp();
+        System.out.println("" + fecha1);
+
+        Timestamp fecha5 = new Timestamp();
+        fecha5.setAño(2024);
+        fecha5.setMes(4);
+        fecha5.setDia(25);
+        fecha5.setHora(0);
+        fecha5.setMinuto(0);
+        fecha5.setSegundo(0);
+        System.out.println(fecha5);
+
+        Timestamp fecha6 = new Timestamp();
+        fecha6.setAño(2024);
+        fecha6.setMes(4);
+        fecha6.setDia(25);
+        fecha6.setHora(12);
+        fecha6.setMinuto(9);
+        fecha6.setSegundo(1);
+        System.out.println(fecha6);
+
+        System.out.println("/n/n/n");
+
+        Timestamp fecha2 = new Timestamp();
+
+        System.out.println("Ingrese el año que desea: ");
+        int año = entrada.nextInt();
+        fecha2.setAño(año);
+
+        boolean entrada1 = true;
+        while (entrada1 == true) {
+            System.out.println("Ingrese el mes que desea: ");
+            int mes = entrada.nextInt();
+            if (mes <= 12 && mes >= 1) {
+                fecha2.setMes(mes);
+                entrada1 = false;
+            } else {
+                entrada1 = true;
+            }
+        }
+
+        Timestamp timestamp = new Timestamp();
+        int diasEnElMes = timestamp.DiadeCadaMes(timestamp.getMes(), timestamp.getAño());
+        entrada1 = true;
+
+        while (entrada1 == true) {
+            System.out.println("Ingrese el día que desea: ");
+            int dia = entrada.nextInt();
+            if (dia >= 1 && dia <= diasEnElMes || dia >= 1 && dia <= 31) {
+                fecha2.setDia(dia);
+                entrada1 = false;
+            } else {
+                entrada1 = true;
+            }
+        }
+
+        System.out.println("\n");
+
+        System.out.println("" + fecha2);
+
+        System.out.println("\n");
+        Timestamp fecha3 = new Timestamp();
+
+        System.out.println("Ingrese el año que desea: ");
+        int año2 = entrada.nextInt();
+        fecha3.setAño(año2);
+
+        entrada1 = true;
+
+        while (entrada1 == true) {
+            System.out.println("Ingrese el mes que desea: ");
+            int mes2 = entrada.nextInt();
+            if (mes2 >= 1 && mes2 <= 12) {
+                fecha3.setMes(mes2);
+                entrada1 = false;
+            } else {
+                System.out.println("Mes inválido");
+                entrada1 = true;
+            }
+        }
+
+        Timestamp timestamp2 = new Timestamp();
+        int diasEnElMes2 = timestamp2.DiadeCadaMes(timestamp.getMes(), timestamp.getAño());
+        entrada1 = true;
+
+        while (entrada1 == true) {
+            System.out.println("Ingrese el día que desea: ");
+            int dia2 = entrada.nextInt();
+            if (dia2 >= 1 && dia2 <= diasEnElMes2 || dia2 >= 1 && dia2 <= 31) {
+                fecha3.setDia(dia2);
+                entrada1 = false;
+            } else {
+                System.out.println("Dia inválido");
+                entrada1 = true;
+            }
+        }
+
+        entrada1 = true;
+        while (entrada1 == true) {
+            System.out.println("Ingrese la hora que desea: ");
+            int hora = entrada.nextInt();
+            if (hora >= 0 && hora <= 23) {
+                fecha3.setHora(hora);
+                entrada1 = false;
+            } else {
+                System.out.println("hora inválida");
+                entrada1 = true;
+            }
+        }
+
+        entrada1 = true;
+        while (entrada1 == true) {
+            System.out.println("Ingrese el minuto que desea: ");
+            int minuto = entrada.nextInt();
+            if (minuto >= 0 && minuto <= 59) {
+                fecha3.setMinuto(minuto);
+                entrada1 = false;
+            } else {
+                System.out.println("Minuto inválido");
+                entrada1 = true;
+            }
+        }
+
+        entrada1 = true;
+        while (entrada1 == true) {
+            System.out.println("Ingrese el segundo que desea: ");
+            int segundo = entrada.nextInt();
+            if (segundo >= 0 && segundo <= 59) {
+                fecha3.setHora(segundo);
+                entrada1 = false;
+            } else {
+                System.out.println("Segundo inválido");
+                entrada1 = true;
+            }
+        }
+
+        System.out.println("" + fecha3);
+
+    }
+}
---
+++ Tarea_Timestamp/src/tarea_timestamp/Timestamp.java
@@ -0,0 +1,183 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tarea_timestamp;
+
+/**
+ *
+ * @author eliza
+ */
+public class Timestamp {
+    int año;
+    int mes;
+    int dia;
+    int hora;
+    int minuto;
+    int segundo;
+    
+    public Timestamp(){
+        this.año = 0;
+        this.mes = 0;
+        this.dia = 0;
+        this.hora = 0;
+        this.minuto = 0;
+        this.segundo = 0;
+    }
+    
+    public Timestamp(int año, int mes, int dia){
+        this.año = año;
+        this.mes = mes;
+        this.dia = dia;
+        this.hora = 0;
+        this.minuto = 0;
+        this.segundo = 0;
+    }
+    
+    public Timestamp(int año,int mes, int dia, int hora, int minuto, int segundo){
+        this.año = año;
+        this.mes = mes;
+        this.dia = dia;
+        this.hora = hora;
+        this.minuto = minuto;
+        this.segundo = segundo;
+    }
+    
+    public int getAño() {
+        return año;
+    }
+    
+    public int getMes(){
+        return mes;
+    }
+    
+    public int getDia(){
+        return dia;
+    }
+    
+    public int getHora(){
+        return hora;
+    }
+    
+    public int getMinuto(){
+        return minuto;
+    }
+    
+    public int getSegundo(){
+        return segundo;
+    }
+    
+    public void setAño(int año){
+        this.año = año;
+    } 
+    
+    public void setMes(int mes){
+        if (mes >= 1 && mes <= 12) {
+            this.mes = mes;
+            boolean entrada = true;
+        } else{
+            System.out.println("Mes inválido.");
+            boolean entrada = false;
+        }
+    }
+    
+    public void setDia(int dia) {
+        if (dia >= 1 && dia <= DiadeCadaMes(this.mes, this.año)) {
+            this.dia = dia;
+            boolean entrada = true;
+        } else {
+            System.out.println("Día inválido.");
+            boolean entrada = false;
+        }
+    }
+
+    
+    public void setHora(int hora) {
+        if (hora >= 0 && hora <= 23) {
+            this.hora = hora;
+            boolean entrada = true;
+        } else {
+            System.out.println("Hora inválida.");
+            boolean entrada = false;
+        }
+    }
+    
+    public void setMinuto(int minuto) {
+        if (minuto >= 0 && minuto <= 59) {
+            this.minuto = minuto;
+            boolean entrada = true;
+        } else { 
+            System.out.println("Minutos inválidos.");
+            boolean entrada = false;
+        }
+    }
+    
+    public void setSegundo(int segundo) {
+        if (segundo >= 0 && segundo <= 59) {
+            this.segundo = segundo;
+            boolean entrada = true;
+        } else { 
+            System.out.println("Segundos inválidos");
+            boolean entrada = false;
+        }
+    }
+    
+    public int DiadeCadaMes(int mes, int año) {
+        switch (mes) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                return 31;
+            case 4: case 6: case 9: case 11:
+                return 30;
+            case 2:
+                if ((año % 4 == 0 && año % 100 != 0) || año % 400 == 0) {
+                    return 29;
+                } else {
+                    return 28;
+                }
+            default:
+                return -1; 
+        }
+    }
+    
+    @Override
+    public String toString() {
+        String ceros;
+        if (mes < 10) {
+            ceros = "0";
+        } else {
+            ceros = "";
+        }
+
+        String ceros2;
+        if (dia < 10) {
+            ceros2 = "0";
+        } else {
+            ceros2 = "";
+        }
+
+        String ceros3;
+        if (hora < 10) {
+            ceros3 = "0";
+        } else {
+            ceros3 = "";
+        }
+
+        String ceros4;
+        if (minuto < 10) {
+            ceros4 = "0";
+        } else {
+            ceros4 = "";
+        }
+
+        String ceros5;
+        if (segundo < 10) {
+            ceros5 = "0";
+        } else {
+            ceros5 = "";
+        }
+        
+        return "" + año + "-" + ceros+ mes + "-" + ceros2 + dia + " " + ceros3 + hora + ":" + ceros4 + minuto + ":" + ceros5 + segundo + "";
+    }
+    
+    
+}
